c2e4cb75797bd656c929bcaeb91d5ef11d5b9529
Marc von Holzen <marc@ookla.com>
2014-02-20 11:17:48 -0800

    Add inputrc Add functional install.sh Add subl as a symlink to SublimeText

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-INSTALL
-=======
-Either symlinks or copy
+#!/bin/sh
+
+# Symlinks dot files to files in the current working directory
 
 src=`pwd`
 ln -sf $src/bash_profile.sh ~/.bash_profile
 ln -sf $src/bash_profile.d ~/.bash_profile.d
 ln -sf $src/bash_non_interactive.sh ~/.bash_non_interactive
 ln -sf $src/bash_non_interactive.d ~/.bash_non_interactive.d
+ln -sf $src/inputrc ~/.inputrc
+
