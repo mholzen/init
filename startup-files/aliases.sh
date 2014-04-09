33c5c4a015f32159ad2c9bee1a24894f9f33cd58
Marc von Holzen <marc@ookla.com>
2014-04-09 15:04:12 -0700

    Alias pushd and popd

--- startup-files/aliases.sh
+++ startup-files/aliases.sh
@@ -1,12 +1,14 @@
 # Bash
-alias h='history'
+alias h=history
 alias chrome='open -a /Applications/Google\ Chrome.app'
 alias open-with-chrome='chrome'
 alias reveal=which
 alias functions='declare -f'
 alias python-debug='python -m pdb'
-alias debug-python='python-debug'
+alias debug-python=python-debug
 alias gs='git status'
+alias pd=pushd
+alias po=popd
 
 # Copy the current working directory so that I can paste or use pbpaste with it
 alias copy-cwd='echo -n `pwd` | pbcopy'
