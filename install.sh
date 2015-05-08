b57f6ed1aac7abd2d91856d191713d1fb95cbe3a
marc@home <marc@vonholzen.org>
2015-05-08 03:35:51 -0700

    Fix install script

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ test -d $dir || echo "cannot locate the source directory"
 
 function uninstall() {
   local file=$1
-  test ! -r $file || return;
-  test -h $file && rm $file;
-  test ! -r $file || ( echo "cannot uninstall $file"; exit 1 )
+  if [ ! -r $file ]; then return; fi
+  if [ -h $file ]; then rm $file; fi
+  if [ -r $file ]; then ( echo "cannot uninstall $file"; exit 1 ); fi
 }
 function install() {
   local file=$1
@@ -19,7 +19,7 @@ install ~/.use-bash
 source $dir/files.sh || exit "cannot locate installed directory"
 
 for file in ${files[*]}; do
-        target=~/.$file
-        if [ -h $target ]; then rm $target; fi
-	if [ ! -d $target ]; then ln -sf $dir/$file $target; fi
+  target=~/.$file
+  if [ -h $target ]; then rm $target; fi
+	if [ ! -d $target ]; then ln -sf ~/.use-bash/$file $target; fi
 done
