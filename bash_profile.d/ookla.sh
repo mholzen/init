da0df408fd8294b36f1de99e5d0fe5d1b7405a7f
Marc von Holzen <marc@ookla.com>
2015-09-18 16:03:30 -0700

    Don't need it.

--- bash_profile.d/ookla.sh
+++ bash_profile.d/ookla.sh
@@ -7,5 +7,3 @@ export PROJECT_BASE=$DEV
 export POCO_BASE=$PROJECT_BASE/poco
 
 PATH=$PATH:$DEV/speedtest-as3-tests/bin:$DEV/speedtest-as3-core/bin
-
-PATH=$DEV/speedtestnet/vendor/phpunit/phpunit/:$PATH
