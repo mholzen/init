cca3c41cac476296925c6aacef9278cd32487ae5
Marc von Holzen <marc@vonholzen.org>
2019-01-21 09:33:59 -0800

    add system to path

--- bash_profile.d/ordered/path.sh
+++ bash_profile.d/ordered/path.sh
@@ -1,7 +1,6 @@
 PATH=$HOME/develop/mholzen/log/bin:$PATH
-PATH=$HOME/develop/mholzen/search/bin:$PATH
+PATH=$HOME/develop/mholzen/system/bin:$PATH
 PATH=$HOME/develop/mholzen/visualize/bin:$PATH
-PATH=$HOME/develop/mholzen/transform/bin:$PATH
 PATH=$HOME/.bash/bin:$PATH
 
 export PATH
