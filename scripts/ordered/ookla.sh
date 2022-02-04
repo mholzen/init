a5e05cc75dcc10b0de83920dc5d21b41de6d7567
Marc von Holzen <marc@ookla.com>
2022-02-04 11:08:06 -0800

    update path for ookla

--- scripts/ordered/ookla.sh
+++ scripts/ordered/ookla.sh
@@ -5,8 +5,6 @@ export VAGRANT_CWD="$DEV/vagrant/development"
 export PROJECT_BASE=$DEV
 export POCO_BASE=$PROJECT_BASE/poco
 
-PATH=$PATH:$DEV/speedtest-as3-tests/bin:$DEV/speedtest-as3-core/bin
-
 # Android development
 #export ANDROID_HOME=${HOME}/develop/speedtestnet-android/toolchain/sdk
 #export PATH="$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/build-tools/android-4.2.2:${HOME}/develop/speedtestnet-android/speedtest"
@@ -17,3 +15,6 @@ export PATH="$PATH:${HOME}/develop/teamookla/android-sdk-macosx/platform-tools"
 PATH=$PATH:$DEV/speedtestnet/vendor/phpunit/phpunit
 
 PATH=$PATH:$DEV/jira/
+
+# Embedded
+PATH=$PATH:$DEV/native-artifacts/speedtest-embedded/latest
--- scripts/ordered/path.sh
+++ scripts/ordered/path.sh
@@ -1,7 +1,6 @@
 PATH=./bin:\
 $HOME/develop/mholzen/log/bin:\
 $HOME/develop/mholzen/system/bin:\
-$HOME/develop/mholzen/visualize/bin:\
 $HOME/.bash/bin:$PATH
 
 export PATH
