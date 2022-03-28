9e76927ec4664db2a0b84ea9d001cb1950ab14db
Marc von Holzen <marc@ookla.com>
2022-03-28 10:26:08 -0700

    keyboard, ordered

--- scripts/ordered/ookla.sh
+++ scripts/ordered/ookla.sh
@@ -6,10 +6,11 @@ export PROJECT_BASE=$DEV
 export POCO_BASE=$PROJECT_BASE/poco
 
 # Android development
+if [ -r ~/Library/Android/sdk/platform-tools/ ]; then
+  PATH="$PATH:$HOME/Library/Android/sdk/platform-tools/"
+fi
 #export ANDROID_HOME=${HOME}/develop/speedtestnet-android/toolchain/sdk
 #export PATH="$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/build-tools/android-4.2.2:${HOME}/develop/speedtestnet-android/speedtest"
-
-export PATH="$PATH:${HOME}/develop/teamookla/android-sdk-macosx/platform-tools"
 
 # Speedtestnet
 PATH=$PATH:$DEV/speedtestnet/vendor/phpunit/phpunit
