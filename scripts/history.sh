029dece2af99d49364313d0b3a97a53c19ca51cd
Marc von Holzen <marc@vonholzen.org>
2023-06-29 10:46:15 -0700

    export hist env

--- scripts/history.sh
+++ scripts/history.sh
@@ -1,6 +1,6 @@
-HISTFILE=~/.zsh_history
-HISTSIZE=1000000
-SAVEHIST=1000000
+export HISTFILE=~/.zsh_history
+export HISTSIZE=1000000
+export SAVEHIST=1000000
 
 setopt inc_append_history # append to history after every command (default is to save when terminal exits)
 setopt share_history
