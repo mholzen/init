fb52035c4b86c25a2ce02948d537829fe234a716
Marc von Holzen <marc@ookla.com>
2023-06-29 10:46:41 -0700

    update

--- scripts/go.sh
+++ scripts/go.sh
@@ -3,5 +3,6 @@ export PATH="/usr/local/go/bin:$PATH"
 # export GOPATH="${HOME}/.go" # update path as wanted
 # export GOROOT="$(brew --prefix golang)/libexec"
 # export GOROOT="/usr/local/opt/go/libexec"
+export GOROOT="/usr/local/go/"
 export GOPRIVATE="github.com/teamookla/*" # required with v >1.13
 # export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
