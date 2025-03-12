3a587fd49e2ab2b78ea232a24b464294a65b6e16
Marc von Holzen <marc@vonholzen.org>
2025-03-11 19:03:42 -0700

    psa, python, add find-recent

--- scripts/ordered/python.sh
+++ scripts/ordered/python.sh
@@ -1,6 +1,2 @@
-# Python MacPorts
-export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/
-
-# Python
-export PYTHONSTARTUP=$HOME/.pythonstartup.py
-
+# Python Homebrew
+export PATH=$PATH:$(brew --prefix python)/libexec/bin
