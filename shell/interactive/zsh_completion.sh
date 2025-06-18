8d9bdb747f4bf607233511e76952c3d8553955da
Marc von Holzen <marc@vonholzen.org>
2025-06-18 11:48:50 -0700

    add just completions

--- shell/interactive/zsh_completion.sh
+++ shell/interactive/zsh_completion.sh
@@ -1,24 +1,16 @@
 #!/bin/zsh
 
-# ZSH completions setup
-# This file sets up ZSH completions for custom commands
-
-# Only run this in ZSH
 if [ -n "$ZSH_VERSION" ]; then
-  # Add functions directory to fpath for completions
-  fpath=($HOME/.bash/functions $fpath)
-  
+  # Add a directory to the function path for autoload
+  fpath=($HOME/.init/shell/functions $fpath)
+
   # Load the base completion system first
   autoload -Uz compinit
   compinit
-  
+
   # Load required completion functions
   autoload -Uz _describe
-  
-  # Now load our custom completion functions
-  if [[ -f $HOME/.bash/functions/_cdm ]]; then
-    # Instead of autoloading, source the file directly to ensure it's properly loaded
-    source $HOME/.bash/functions/_cdm
-    compdef _cdm cdm
-  fi
-fi 
+
+  compdef _cdm cdm
+  compdef _just just
+fi
