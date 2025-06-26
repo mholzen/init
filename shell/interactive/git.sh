878638831c10dc523ffddf2ad2131637fa56859c
Marc von Holzen <marc@vonholzen.org>
2025-06-26 09:21:42 -0700

    use cursor for git editor when in cursor

--- shell/interactive/git.sh
+++ shell/interactive/git.sh
@@ -5,6 +5,12 @@ function cdr() {
   fi
 }
 
+# Configure git editor based on environment
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+    # When in VSCode terminal, use VSCode as git editor
+    export GIT_EDITOR="cursor --wait"
+fi
+
 alias gs='git status'
 alias gb='git branch'
 alias gd='git diff'
