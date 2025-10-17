a52a6921046ae6a3f68f92809231f26f0550a1e4
Marc von Holzen <marc@vonholzen.org>
2025-10-17 14:53:29 -0700

    add python binaires added by pip3

--- shell/interactive/ordered/python.sh
+++ shell/interactive/ordered/python.sh
@@ -2,3 +2,8 @@ if command -v brew >/dev/null 2>&1; then
     # Python Homebrew
     export PATH=$PATH:$(brew --prefix python)/libexec/bin
 fi
+
+python_base=$(python3 -m site --user-base)
+if [ -d "$python_base/bin" ]; then
+    export PATH=$PATH:$python_base/bin
+fi
